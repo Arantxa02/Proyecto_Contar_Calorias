122b3434b9d2abf34f6d4340928fe13d8593249e
Arantxa02 <Arantxa200201@gmail.com>
2024-04-23 21:26:12 +0100

    first-Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {  View } from 'react-native';
+
+import Home from './src/Screens/Home/Home';
+
+export default function App() {
+  return (
+    <View>
+      <Home/>
+    </View>
+  );
+}
+
+
